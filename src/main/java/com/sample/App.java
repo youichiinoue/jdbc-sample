ccb4b7431b53578e12d23a7e2fe17aca506c37a5
yoichi inoue <yoichi_inoue@ewest.co.jp>
2016-12-13 13:23:15 +0900

    data access

--- src/main/java/com/sample/App.java
+++ src/main/java/com/sample/App.java
@@ -3,6 +3,11 @@ package com.sample;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.sample.model.Todo;
+import com.sample.model.TodoDao;
+
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -12,6 +17,10 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        System.out.println( "Hello World!" );
+        TodoDao todoDao = (TodoDao) ctx.getBean("todoDaoImpl");
+        todoDao.createTodo(1, "abc");
+        todoDao.createTodo(2, "def");
+        List<Todo> list = todoDao.getAll();
+        System.out.println( "Hello World!" + list.size() );
     }
 }
